4b7cb19cde0915cf432dae1f12636469c68dda05
nick-stu <1733763428@qq.com>
2018-04-08 11:22:33 +0800

    添加一个判断，即现有类只有一个点时，把平均距离设为0，以便于直接归入该类

--- distanceChange.m
+++ distanceChange.m
@@ -2,9 +2,12 @@ function[distance,avg]=distanceChange(sample,traindata)
     [num,~]=size(traindata);
     total=pdist2(sample,traindata);
     distance=sum(total(:))/ num;
-    
-    data=[traindata;sample];
-    [num,~]=size(data);
-	total=pdist2(data,data); % 这里是平均距离
-    avg = sum(total(:))/(num*(num-1));
+    if num==1   % 如果已有类只有一个点，那选取最近的点加入就行了，不用判断阈值
+        avg=0;
+    else
+        data=[traindata;sample];
+        [num,~]=size(data);
+        total=pdist2(data,data); % 这里是平均距离
+        avg = sum(total(:))/(num*(num-1));
+    end
 end
