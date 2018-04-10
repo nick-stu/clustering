54e6d99cc1cb913830fca18a44e40a63e6d90b91
nick-stu <1733763428@qq.com>
2018-04-10 21:04:02 +0800

    更新NN_New，对数据做featureNormalize,并调用动态化生成训练样本标签的函数getLabelsNew

--- NN_New.m
+++ NN_New.m
@@ -1,20 +1,13 @@
-function [accuracy, net ] = NN_New( trainData1,trainData2, testData, testLabel )
+function [accuracy, net ] = NN_New( trainData, testData, testLabel )
 % 使用神经网络
 
+trainData = featureNormalize(trainData);
+testData = featureNormalize(testData);
 % 生成训练标签
-rows = size(trainData1, 1);
-singleNum = rows / 9;
-[trainLabel1,tag1]=getLabelsNew(trainData1,singleNum,3);
+[trainLabel,tag] = getLabelsNew(trainData);
 
-rows = size(trainData2, 1);
-singleNum = rows / 9;
-[trainLabel2,tag2]=getLabelsNew(trainData2,singleNum,3);
-
-trainLabel=[trainLabel1;trainLabel2];
-trainData=[trainData1;trainData2];
-tag=[tag1 tag2+max(tag1)];
 % 训练网络
-net = NNTrainNew(trainData, trainLabel);
+net = NNTrainNew(trainData, trainLabel,size(tag,2));
 
 % 仿真
 Y = sim( net , testData' );
@@ -28,7 +21,7 @@ for i = 1 : s
     if m > 0
         index=tag(index);
         validNum = validNum + 1;
-        if((size(testLabel, 2) == 9 && testLabel(i, index) == 1) || index  == testLabel(i))
+        if((size(testLabel, 2) == 9 && testLabel(i, index) == 1))% || index  == testLabel(i))
             correctNum = correctNum + 1 ; 
         end
     else
