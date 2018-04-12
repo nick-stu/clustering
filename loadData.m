4e61d33a4cefd264691e2435ae08d8dcc9881d49
nick-stu <1733763428@qq.com>
2018-04-12 16:24:32 +0800

    提交loadData函数，最新版

--- loadData.m
+++ loadData.m
@@ -3,7 +3,7 @@ warning off;
 addpath(genpath(pwd));
 basePath = './大力小力/';
 dirs = dir(basePath);
-begin=30*0 + 1;
+begin=30*8 + 1;
 tail=begin+29;
 for x=1:size(dirs, 1)
     if (dirs(x).name(1) == '.')
@@ -11,12 +11,15 @@ for x=1:size(dirs, 1)
     end
     fprintf('%s-> ', dirs(x).name);
     
-    load([basePath, dirs(x).name, '/hard/harddecimate_data_0.6k.mat']);
+    load([basePath, dirs(x).name, '/hard/hard_decimatedata_0.6k_withPSD.mat']);
     hard = decimate_data(begin:tail, :);
-    load([basePath, dirs(x).name, '/gentle/gentledecimate_data_0.6k.mat']);
+    load([basePath, dirs(x).name, '/gentle/gentle_decimatedata_0.6k_withPSD.mat']);
     gentle = decimate_data(begin:tail, :);
     data=[hard;gentle];
+    %% 是否归一化
+    originData=data;
+    data=featureNormalize(data);% 归一化
     mdl=clustering_dis(data);
-%     densityTest(data);
-    pcaPlot(data,3,mdl.label);
+%     densitySituation(data);
+    pcaPlot(originData,3,mdl.label);
 end
